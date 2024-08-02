47c46e97e2c9d9b12e6cac7722b8f36dccef932e
Manuelly Carvalho <manuellycarvalho59@gmail.com>
2024-08-02 19:49:38 -0300

    maze game

---
+++ OneDrive/Área de Trabalho/jogoLabirinto/labirintoJogo/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import Maze from './components/Maze';
+
+const App = () => {
+  return (
+    <div>
+      <Maze />
+    </div>
+  );
+};
+
+export default App;
